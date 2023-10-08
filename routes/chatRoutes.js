74b7ececf6b8d9f876e51429ae9c0a8575b71c99
David Val <101377109+Val-senseisama@users.noreply.github.com>
2023-10-08 15:03:00 +0100

    Update chatRoutes.js

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
+router.route("/chat").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
 router.route("/rename").put(protect, renameGroup);
 router.route("/groupremove").put(protect, removeFromGroup);
