71dbb7203d3aad7c889bd746a96bb26c77d95df5
David Val <101377109+Val-senseisama@users.noreply.github.com>
2023-10-08 15:18:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Headers', '*', 'Access-Control-Allow-Origin', 'Origin', 'X-Requested-with', 'Content_Type,Accept,Authorization','http://localhost:5000');
+    res.header('Access-Control-Allow-Headers', '*', 'Access-Control-Allow-Origin', 'Origin', 'X-Requested-with', 'Content_Type,Accept,Authorization','https://th-backend-45458f922f56.herokuapp.com');
     if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
         return res.status(200).json({});
